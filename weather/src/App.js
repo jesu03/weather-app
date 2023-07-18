06da47b38626bc8af26e37c7f631ced118a18d84
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-07-18 12:54:57 +0530

    upload a files

--- weather/src/App.js
+++ weather/src/App.js
@@ -2,18 +2,33 @@ import './App.css';
 import Card from './components/Card.js';
 import { useState } from 'react';
 import axios from 'axios';
+import Result from './components/Result.js';
 function App() {
   const[name,setName] = useState("");
+  const[temp,setTemp] = useState("");
+  const[desc,setDesc] = useState("");
+  const[imgurl,setImgurl] = useState("");
   const findWeather = async () =>{
     const apikey = "1fbe01800f4036e3a260387dc0f557cc";
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=" + apikey + "&units=" + unit;
     const response = await axios.get(url);
-    console.log(response);
+    const temp = response.data.main.temp;
+    const desc = response.data.weather[0].description;
+    const icon = response.data.weather[0].icon;
+    const imgurl =  "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    setTemp(temp);
+    setDesc(desc);
+    setImgurl(imgurl);
+    console.log(desc,icon,imgurl);
   }
   return (
     <div className="App">
-    <Card  name={name} setName={setName} findWeather={findWeather}/>
+    {temp === "" ?(
+      <Card  name={name} setName={setName} findWeather={findWeather} />
+    ):(
+      <Result temp={temp} desc={desc} imgurl={imgurl} setTemp={setTemp}/>
+    )}
     </div>
   );
 }
